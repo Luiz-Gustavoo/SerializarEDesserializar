2c4421b413232d7cb34c55d9a0d286f4d93eff96
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-17 20:20:49 -0300

    Adicionar service Contador

---
+++ src/main/java/br/com/alura/serializardesserializar/services/Contador.java
@@ -0,0 +1,10 @@
+package br.com.alura.serializardesserializar.services;
+
+public class Contador {
+
+    public void contador(int numero) {
+        for(int i = 1; i <= numero; i++) {
+            System.out.println(i);
+        }
+    }
+}
