bea9f029274f8bc5b21cc6f0a5cad0f54893bd3f
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-18 12:40:50 -0300

    Refatorar usando a classe ConverteDados

--- src/main/java/br/com/alura/serializardesserializar/SerializardesserializarApplication.java
+++ src/main/java/br/com/alura/serializardesserializar/SerializardesserializarApplication.java
@@ -1,17 +1,13 @@
 package br.com.alura.serializardesserializar;
 
 import br.com.alura.serializardesserializar.Modelos.Tarefas;
-import br.com.alura.serializardesserializar.services.Contador;
-import br.com.alura.serializardesserializar.services.LerNumero;
+import br.com.alura.serializardesserializar.services.ConverteDados;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.cfg.MapperBuilder;
-import com.google.gson.Gson;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.File;
-import java.io.FileWriter;
 
 @SpringBootApplication
 public class SerializardesserializarApplication implements CommandLineRunner {
@@ -23,30 +19,30 @@ public class SerializardesserializarApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		ObjectMapper mapper = new ObjectMapper();
-		Gson gson = new Gson();
-		File arquivo = new File("tarefas.json");
-		File arquivo2 = new File("tarefas2.txt");
+		File arquivo = new File("Tarefas.json");
 
-		// USANDO JACKSON
-		Tarefas tarefa1 = new Tarefas("Lavar louça", false, "alguém");
-		Tarefas tarefa2 = new Tarefas("Lavar carro", true, "eu");
-		mapper.writeValue(arquivo, tarefa1); // escrever o conteúdo do objeto no arquivo tarefas.json (serializar)
-		mapper.writeValue(arquivo, tarefa2);
+		//DESSERIALIZAR - JSON em um objeto Tarefas
+		String json = "{ \"descricao\": \"ir no mercado\", \"concluida\": true, \"pessoaResponsavel\": \"fulano\" }";
 
-		Tarefas tarefaObj = mapper.readValue(arquivo, Tarefas.class); // transformar o conteúdo do arquivo em um objeto Tarefas (desserializar)
-		System.out.println(tarefaObj);
+		ConverteDados converteDados = new ConverteDados();
+		Tarefas tarefaConvertida = converteDados.desserializar(json, Tarefas.class);
+		System.out.println(tarefaConvertida);
 
-		System.out.println("----------------------------------------------------------------------------------------");
+		//SERIALIZAR - objeto Tarefas em um arquivo JSON
+		String escreverTarefa = converteDados.serializar(arquivo, tarefaConvertida);
 
-		//USANDO GSON
-		Tarefas tarefa3 = new Tarefas("comer", true, "outra pessoa");
 
-		String tarefa3Convertida = gson.toJson(tarefa3);
-		System.out.println(tarefa3Convertida);
 
-		FileWriter escreverArquivo = new FileWriter(arquivo2);
-		escreverArquivo.write(tarefa3Convertida);
-		escreverArquivo.close();
+//		System.out.println("----------------------------------------------------------------------------------------");
+//		//USANDO GSON
+//
+//		Gson gson = new Gson();
+//
+//		Tarefas tarefa3 = new Tarefas("comer", true, "outra pessoa");
+//
+//		String tarefa3Convertida = gson.toJson(tarefa3);
+//		System.out.println(tarefa3Convertida);
+
 
 
 	}
