ba1d088b6fda4d12fe981d8dd71cbee3661686aa
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-17 23:12:28 -0300

    Adicionar serialização e desserialização

--- src/main/java/br/com/alura/serializardesserializar/SerializardesserializarApplication.java
+++ src/main/java/br/com/alura/serializardesserializar/SerializardesserializarApplication.java
@@ -1,10 +1,17 @@
 package br.com.alura.serializardesserializar;
 
+import br.com.alura.serializardesserializar.Modelos.Tarefas;
 import br.com.alura.serializardesserializar.services.Contador;
 import br.com.alura.serializardesserializar.services.LerNumero;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.cfg.MapperBuilder;
+import com.google.gson.Gson;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.FileWriter;
 
 @SpringBootApplication
 public class SerializardesserializarApplication implements CommandLineRunner {
@@ -15,10 +22,32 @@ public class SerializardesserializarApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		LerNumero lerNumero = new LerNumero();
-		int numeroDigitado = lerNumero.lerNumero();
+		ObjectMapper mapper = new ObjectMapper();
+		Gson gson = new Gson();
+		File arquivo = new File("tarefas.json");
+		File arquivo2 = new File("tarefas2.txt");
 
-		Contador contarNumero = new Contador();
-		contarNumero.contador(numeroDigitado);
+		// USANDO JACKSON
+		Tarefas tarefa1 = new Tarefas("Lavar louça", false, "alguém");
+		Tarefas tarefa2 = new Tarefas("Lavar carro", true, "eu");
+		mapper.writeValue(arquivo, tarefa1); // escrever o conteúdo do objeto no arquivo tarefas.json (serializar)
+		mapper.writeValue(arquivo, tarefa2);
+
+		Tarefas tarefaObj = mapper.readValue(arquivo, Tarefas.class); // transformar o conteúdo do arquivo em um objeto Tarefas (desserializar)
+		System.out.println(tarefaObj);
+
+		System.out.println("----------------------------------------------------------------------------------------");
+
+		//USANDO GSON
+		Tarefas tarefa3 = new Tarefas("comer", true, "outra pessoa");
+
+		String tarefa3Convertida = gson.toJson(tarefa3);
+		System.out.println(tarefa3Convertida);
+
+		FileWriter escreverArquivo = new FileWriter(arquivo2);
+		escreverArquivo.write(tarefa3Convertida);
+		escreverArquivo.close();
+
+
 	}
 }
